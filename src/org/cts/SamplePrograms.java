a06f2d36b705a54d0ffb94195f649b87f2bfce13
PriyaKumar14298 <priyavijaykumar14298@gmail.com>
2021-10-11 16:12:42 +0530

    Just Project Push

---
+++ src/org/cts/SamplePrograms.java
@@ -0,0 +1,12 @@
+package org.cts;
+
+public class SamplePrograms {
+
+	public static void main(String[] args) {
+String s = "Java";
+String s1="Java";
+int q = s.compareTo(s1);
+	System.out.println(q);
+	}
+
+}
